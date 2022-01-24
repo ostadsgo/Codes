3d8ecc977e705d369d052beacba0fc3f1dc5a501
Saeid Gholami <saeidxgholami@gmail.com>
2022-01-24 05:39:08 +0330

    create sample data on patient table.

--- patientM/database.py
+++ patientM/database.py
@@ -36,12 +36,40 @@ class Timeslot(db.Entity):
     medicine = pony.Required(Medicine)
 
 
-def run():
+@pony.db_session
+def add_patient(**patient):
+    Patient(**patient)
+
+
+def make_db(filename="patient.db"):
     # create database file
-    db.bind(provider="sqlite", filename="patient.db", create_db=True)
+    db.bind(provider="sqlite", filename=filename, create_db=True)
+    # Trun on debug mode; should be before generate_mapping
+    pony.set_sql_debug(True)
     # create tables in the database
     db.generate_mapping(create_tables=True)
 
 
 if __name__ == "__main__":
-    run()
+    make_db()
+
+    # # Some test
+    # add_patient(
+    #     name="John Doe",
+    #     gender="Male",
+    #     phone="+1 (515) 765 1190",
+    #     date_of_birth=datetime.date(1982, 11, 27),
+    #     address="US, LA, John Hopkins St. N.49",
+    #     email="johndoe@mail.com",
+    #     disease="Covid 19",
+    # )
+
+    # add_patient(
+    #     name="Kate Doe",
+    #     gender="Female",
+    #     phone="+1 (515) 918 7119",
+    #     date_of_birth=datetime.date(1984, 7, 11),
+    #     address="US, LA, John Hopkins St. N.49",
+    #     email="katedoe@mail.com",
+    #     disease="Covid 19",
+    # )
