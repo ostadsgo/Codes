195079088739dd9cfa8f5d721c40c70409288558
Saeid Gholami <saeidxgholami@gmail.com>
2022-01-30 21:32:29 +0330

    bootstrap blog app

--- patientM/core.py
+++ patientM/core.py
@@ -10,6 +10,18 @@ class Patient:
         patient_id = db.create(db.Patient, self.kw)
         return patient_id
 
-    @classmethod
-    def convert_to_date(cls, date_str):
-        return datetime.strptime(date_str, "%Y/%m/%d")
+
+class Medicine:
+    def __init__(self, **kw):
+        self.kw = kw
+
+    def create(self):
+        med_id = db.create(db.Medicine, self.kw)
+        return med_id
+
+
+# --------------------
+# Helper functions.
+# --------------------
+def convert_to_date(cls, date_str):
+    return datetime.strptime(date_str, "%Y/%m/%d")
--- patientM/ui.py
+++ patientM/ui.py
@@ -14,7 +14,7 @@ import tkcalendar as tkcal
 import core
 
 
-class PatientForm(ttk.Frame):
+class PatientFrame(ttk.Frame):
     def __init__(self, master, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
         self.ui()
@@ -28,6 +28,7 @@ class PatientForm(ttk.Frame):
         # row 1
         ttk.Label(self, text="Gender ").pack()
         self.gender = ttk.Combobox(self, values=["Female", "Male", "Other"])
+        self.gender.set("Female")
         self.gender.pack()
 
         # row 2
@@ -36,7 +37,7 @@ class PatientForm(ttk.Frame):
         self.phone.pack()
 
         # row 3
-        ttk.Label(self, text="Date of Bright (yyyy/mm/dd)").pack()
+        ttk.Label(self, text="Date of Bright").pack()
         self.date_of_birth = tkcal.DateEntry(
             self,
             width=12,
@@ -62,14 +63,10 @@ class PatientForm(ttk.Frame):
         self.disease = ttk.Entry(self)
         self.disease.pack()
 
-        # row 7 - submit button
-        ttk.Button(self, text="Submit", command=self.on_submit).pack()
-
-        # config all widgets
         for child in self.winfo_children():
             child.pack_configure(fill=tk.BOTH, expand=True)
             if child.widgetName == "ttk::label":
-                child.pack_configure(pady=(10, 0))
+                child.pack_configure(pady=(20, 0))
 
     # Clear form entry fields
     def clear_fields(self):
@@ -90,8 +87,53 @@ class PatientForm(ttk.Frame):
         )
         return patient
 
+
+class MedicineFrame(ttk.Frame):
+    def __init__(self, master, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        self.timeslot_num = 3
+        self.row_num = 5
+        self.rows = []
+        self.ui()
+
+    def _header(self):
+        frame = ttk.Frame(self, relief="sunken", padding=(5, 5))
+        frame.pack(fill=tk.X)
+        titles = {"Medicine": 160, "Timeslot 1": 20, "Timeslot 2": 20, "Timeslot3": 20}
+        for title, pad in titles.items():
+            ttk.Label(frame, text=title).pack(side=tk.LEFT, fill=tk.X, padx=(0, pad))
+
+    def _medicine_table(self, row_num=5):
+        # size of entries. 30 for medicine 10 for timeslots
+        col_sizes = (30,) + (10,) * self.timeslot_num
+        for _ in range(row_num):
+            frame = ttk.Frame(self, padding=(5, 3))
+            frame.pack(fill=tk.X)
+            row = []
+            for size in col_sizes:
+                e = ttk.Entry(frame, width=size)
+                e.pack(side=tk.LEFT, fill=tk.X, padx=(0, 5))
+                self.rows.append(e)
+
+    def ui(self):
+        self._header()
+        self._medicine_table()
+
+
+class PatientForm(tk.Toplevel):
+    def __init__(self, master, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        # Patient register frame
+        pati_frame = PatientFrame(self)
+        pati_frame.pack(side=tk.LEFT)
+
+        # Medicine table frame
+        style = ttk.Style(self)
+        style.configure("medicine.TFrame", background="#457B9D")
+        med_frame = MedicineFrame(self, style="medicine.TFrame")
+        med_frame.pack()
+
+        ttk.Button(self, text="Submit", command=self.on_submit).pack()
+
     def on_submit(self):
-        patient = self.get_patient()
-        patient.register()
-        self.clear_fields()
-        print("The patient registerd")
+        print("on submit pressed!")
