54ad77db68480041eff8306ce8b1c1840db46429
Saeed Gholami <saeed.ghollami@gmail.com>
2023-04-19 18:08:43 +0330

    start dart

--- python/kidscancode/GameDevWithPython/jumpy/main.py
+++ python/kidscancode/GameDevWithPython/jumpy/main.py
@@ -1,38 +1,33 @@
-# Jumpy platform game.
 import pygame as pg
+import random
 from settings import *
 from sprite import *
 
 
 class Game:
     def __init__(self):
+        # initialize game window, etc
         pg.init()
         pg.mixer.init()
-        self.screen = pg.display.set_mode(SCREEN_SIZE)
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
         self.running = True
-        # sprite groups
-        self.all_sprites = pg.sprite.Group()
-        self.paltforms = pg.sprite.Group()
 
-    # start new game if you lose.
     def new(self):
-        # make a player
-        self.player = Player()
+        # start a new game
+        self.all_sprites = pg.sprite.Group()
+        self.platforms = pg.sprite.Group()
+        self.player = Player(self)
         self.all_sprites.add(self.player)
-        # make a platform
-        p1 = Platform(0, HEIGHT - 40, WIDTH, 60)
-        p2 = Platform(WIDTH / 2, 450, 100, 10)
-        self.all_sprites.add(p1)
-        self.all_sprites.add(p2)
-        self.paltforms.add(p1)
-        self.paltforms.add(p2)
-        # run the game
+        for plat in PLATFORM_LIST:
+            p = Platform(*plat)
+            self.all_sprites.add(p)
+            self.platforms.add(p)
         self.run()
 
-    # Game loop
     def run(self):
+        # Game Loop
         self.playing = True
         while self.playing:
             self.clock.tick(FPS)
@@ -41,34 +36,47 @@ class Game:
             self.draw()
 
     def update(self):
+        # Game Loop - Update
         self.all_sprites.update()
-        hits = pg.sprite.spritecollide(self.player, self.paltforms, False)
-        if hits:
-            self.player.pos.y = hits[0].rect.top
-            self.player.vel.y = 0
+        # check if player hits a platform - only if falling
+        if self.player.vel.y > 0:
+            hits = pg.sprite.spritecollide(self.player, self.platforms, False)
+            if hits:
+                self.player.pos.y = hits[0].rect.top
+                self.player.vel.y = 0
 
     def events(self):
+        # Game Loop - events
         for event in pg.event.get():
+            # check for closing window
             if event.type == pg.QUIT:
                 if self.playing:
                     self.playing = False
                 self.running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    self.player.jump()
 
     def draw(self):
+        # Game Loop - draw
         self.screen.fill(BLACK)
         self.all_sprites.draw(self.screen)
+        # *after* drawing everything, flip the display
         pg.display.flip()
 
     def show_start_screen(self):
+        # game splash/start screen
         pass
 
     def show_go_screen(self):
+        # game over/continue
         pass
 
 
-game = Game()
-game.show_start_screen()
-while game.running:
-    game.new()
-    game.show_start_screen()
+g = Game()
+g.show_start_screen()
+while g.running:
+    g.new()
+    g.show_go_screen()
+
 pg.quit()
