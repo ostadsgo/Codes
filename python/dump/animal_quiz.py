437f04905da37e58238b16b1d602eed0381f6484
Saeed Gholami <saeed.ghollami@gmail.com>
2024-02-27 20:28:34 +0330

    Start to read invent your own games with python.

--- python/dump/animal_quiz.py
+++ python/dump/animal_quiz.py
@@ -1,13 +1,32 @@
+def check_answer(guess, corr_ans):
+    global score
+    for _ in range(2):
+        ans = input(guess)
+        if corr_ans.lower() == ans.lower():
+            print("Correct answer")
+            score += 1
+            return True
+        else:
+            q = "Sorry, wrong answer. Try again. "
+            return check_answer(q, ans)
+
+    return False
+
+
 print("Guess the Animal!")
 score = 0
 
 
-for i in range(2):
+q = "Which bear lives in the North Pole"
+if check_answer(q, "polar bear"):
+    print("Correct answer")
+    score += 1
+else:
     ans = input("Sorry, wrong answer. Try again. ")
-    if ans == "polar bear":
+    if check_answer(q, "polar bear"):
         print("Correct answer")
         score += 1
-        break
+
 
 for i in range(2):
     ans = input("Sorry, wrong answer. Try again. ")
