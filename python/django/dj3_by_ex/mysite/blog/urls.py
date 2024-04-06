68add64ed0d3cebe09fb278cec9e567f1e79e44a
Saeed Gholami <saeed.ghollami@gmail.com>
2024-04-06 23:20:29 +0330

    Display posts on web page.

--- python/django/dj3_by_ex/mysite/blog/urls.py
+++ python/django/dj3_by_ex/mysite/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
 
+from . import views
 
 app_name = "blog"
 
@@ -8,7 +8,7 @@ app_name = "blog"
 urlpatterns = [
     path("", views.post_list, name="post_list"),
     path(
-        "<int: year>/<int:month>/<int:day>/<slug:post>/",
+        "<int:year>/<int:month>/<int:day>/<slug:post>/",
         views.post_detail,
         name="post_detail",
     ),
--- python/django/dj3_by_ex/mysite/blog/views.py
+++ python/django/dj3_by_ex/mysite/blog/views.py
@@ -1,11 +1,22 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, "blog/post/list.html", {"posts": posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get("page")
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(Paginator.num_pages)
+
+    context = {"posts": posts, "page": page}
+    return render(request, "blog/post/list.html", context)
 
 
 def post_detail(request, year, month, day, post):
