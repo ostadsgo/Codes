007a41d9526d9db471865e5a0491072ca3f0db81
Saeed Gholami <saeed.ghollami@gmail.com>
2024-04-08 16:59:50 +0330

    Pagination fish ch01; dj3 by example.

--- python/django/dj3_by_ex/mysite/blog/views.py
+++ python/django/dj3_by_ex/mysite/blog/views.py
@@ -1,22 +1,29 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 
 from .models import Post
 
 
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)
-    page = request.GET.get("page")
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(Paginator.num_pages)
+# def post_list(request):
+#     object_list = Post.published.all()
+#     paginator = Paginator(object_list, 3)
+#     page = request.GET.get("page")
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(Paginator.num_pages)
+#
+#     context = {"posts": posts, "page": page}
+#     return render(request, "blog/post/list.html", context)
 
-    context = {"posts": posts, "page": page}
-    return render(request, "blog/post/list.html", context)
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
 
 
 def post_detail(request, year, month, day, post):
