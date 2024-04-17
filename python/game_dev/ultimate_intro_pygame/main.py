94c2c72cd214b15a67f22952f265851b30f22d4f
Saeed Gholami <saeed.ghollami@gmail.com>
2024-04-17 22:03:33 +0330

    Create jump system for the game.

--- python/game_dev/ultimate_intro_pygame/main.py
+++ python/game_dev/ultimate_intro_pygame/main.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import pygame as pg
 
 TITLE = "My Game"
-
 WIDTH = 800
 HEIGHT = 450
 SCREEN_SIZE = (WIDTH, HEIGHT)
@@ -12,6 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent
 IMAGE_DIR = BASE_DIR / "img"
 SOUND_DIR = BASE_DIR / "snd"
 FONT_DIR = BASE_DIR / "font"
+GROUND = HEIGHT - 150
 
 pg.init()
 screen = pg.display.set_mode(SCREEN_SIZE)
@@ -27,12 +27,13 @@ score_rect = score.get_rect(center=(WIDTH // 2, 50))
 sky = pg.image.load(IMAGE_DIR / "Sky.png")
 ground = pg.image.load(IMAGE_DIR / "ground.png").convert_alpha()
 snail = pg.image.load(IMAGE_DIR / "snail" / "snail1.png").convert_alpha()
-snail_rect = snail.get_rect(midbottom=(WIDTH - 100, HEIGHT - 150))
+snail_rect = snail.get_rect(midbottom=(WIDTH - 100, GROUND))
 snail_x = 600
 
 # Player
 player = pg.image.load(IMAGE_DIR / "Player" / "player_walk_1.png")
-player_rect = player.get_rect(midbottom=(100, HEIGHT - 150))
+player_rect = player.get_rect(midbottom=(100, GROUND))
+player_gravity = 0
 
 running = True
 while running:
@@ -40,21 +41,27 @@ while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_SPACE and player_rect.bottom == GROUND:
+                    player_gravity = -20
 
     screen.fill((0, 0, 0))
-    # Showing graphics
+    # ----- Showing graphics -----
     screen.blit(sky, (0, 0))
     pg.draw.rect(screen, "#c0e8ec", score_rect)
     screen.blit(score, score_rect)
-    screen.blit(ground, (0, HEIGHT - 150))
-    # snail
+    screen.blit(ground, (0, GROUND))
+    # ----- snail -----
     screen.blit(snail, snail_rect)
     snail_rect.x -= 5
     if snail_rect.left < -50:
         snail_rect.left = WIDTH + 50
-
-    # player
+    # ------ player ------
     screen.blit(player, player_rect)
+    player_gravity += 1
+    player_rect.y += player_gravity
+    if player_rect.bottom > GROUND:
+        player_rect.bottom = GROUND
 
     # collision between player and snail
 
