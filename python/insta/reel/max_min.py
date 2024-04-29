507fc52f62b0a16bec35e1c3ec8ca643b9dbe97a
Saeed Gholami <saeed.ghollami@gmail.com>
2024-04-29 19:04:11 +0330

    Max Min

--- python/insta/reel/max_min.py
+++ python/insta/reel/max_min.py
@@ -1,7 +1,16 @@
 
+def say_hello(name: str) -> str:
+    return f"Hello {name}"
 
 names = ["Joe", "Albert", "Bob"]
 
+x = 0
+if x > 0:
+    print("something")
+elif x < 0:
+    print("Another thing.")
+else:
+    print("Nothing")
 
 print(max(names, key=len))
 print(min(names, key=len))
