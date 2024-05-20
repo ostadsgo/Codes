cc397eb8c4d5a13095e6e8ef5f7c62bbb366ade3
Saeed Gholami <saeed.ghollami@gmail.com>
2024-05-20 22:09:33 +0330

    studybud first 1.5 hrs

--- python/dump/area.py
+++ python/dump/area.py
@@ -1,10 +1,12 @@
 # Calculate area of a rectangle.
 
 try:
+
     width = int(input("Enter width: "))
     height = int(input("Enter height: "))
     area = width * height
-    print("Area is", area)
+    print( "Area is", area)
+
 except ValueError:
     print("Please enter interger value.")
 
