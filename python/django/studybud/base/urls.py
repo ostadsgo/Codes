38bda2d14f24c085cbc8ef779ad6eeeb6654c290
Saeed Gholami <saeed.ghollami@gmail.com>
2024-05-23 08:42:12 +0330

    CRUD operation for room

--- python/django/studybud/base/urls.py
+++ python/django/studybud/base/urls.py
@@ -6,4 +6,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("room/<int:pk>/", views.room, name="room"),
+    path("create-room/", views.create_room, name="create-room"),
+    path("update-room/<int:pk>/", views.update_room, name="update-room"),
+    path("delete-room/<int:pk>/", views.delete_room, name="delete-room"),
 ]
