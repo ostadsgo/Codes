b7c9223e9163dca6fe312967df772845aecef06d
Saeed Gholami <saeed.ghollami@gmail.com>
2025-02-02 00:12:26 +0330

    update

--- python/django/dump/mywebsite/home/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-def show_home(request):
-    name = "bob"
-    return render(request, "home.html", context={"name": name})
--- python/django/test_django_project/test_project/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.PostList.as_view()),
-    path("<slug:slug>/", views.PostDetailView.as_view(), name="post_detail"),
-]
